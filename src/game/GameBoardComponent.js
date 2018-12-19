6fc05fd6e936824e166e57ba2c1f16d27dee0654
unknown <johansson.henrik89@gmail.com>
2018-12-19 14:15:31 +0100

    history + refactor

--- src/game/GameBoardComponent.js
+++ src/game/GameBoardComponent.js
@@ -1,50 +1,20 @@
 import React, { Component } from 'react';
 import Chessground from 'react-chessground';
 import 'react-chessground/dist/styles/chessground.css';
-import Chess from 'chess.js';
-
+import gameStore from '../store/GameStore';
 
 class GameBoardComponent extends Component {
-  chess = new Chess()
-
-  state = {
-    fen: ''
-  }
-
   onComponentDidMount() {
 
   }
-
-  checkTurnColor = () => {
-    return this.chess.turn() === 'w' ? 'white' : 'black';
-  }
-
-  onMove(from, to) {
-    if (this.chess.move({ from, to })) {
-
-
-      console.log(this.chess.moves())
-      console.log(this.chess.history())
-    } else {
-      console.log("invalid")
-    }
-    this.setState({
-      fen: this.chess.fen()
-    })
-
-  }
-
   render() {
-    console.log(this.chess.moves())
-    console.log(this.chess.history())
-
     return (
       <div>
         GameBoardComponent
         <Chessground
-          onMove={(from, to) => this.onMove(from, to)}
-          fen={this.state.fen}
-          turnColor={this.checkTurnColor()} />
+          fen={this.props.fen}
+          onMove={(from, to) => gameStore.onMove(from, to)}
+          turnColor={gameStore.checkTurnColor()} />
       </div>
     );
   }
