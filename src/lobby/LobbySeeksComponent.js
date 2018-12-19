77551e109551014a58069e376787559cdccea023
Nalfs <nadim.al.sharif@gmail.com>
2018-12-19 14:30:59 +0100

    init

--- src/lobby/LobbySeeksComponent.js
+++ src/lobby/LobbySeeksComponent.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+let newComp = 'Detta är en komponent';
+
+
 class LobbySeeksComponent extends Component {
+
   render() {
     return (
       <div>
         <div>
-            Seeks
+            <p>Seeks</p>
+            {this.newComp}
         </div>
       </div>
     );
