706a99350a6f179b7c251f73d03fd58c2630d42c
Daniel <shamaran85@gmail.com>
2018-12-20 21:26:07 +0100

    Created Front-End with MOCK-data

--- src/game/GameChatComponent.js
+++ src/game/GameChatComponent.js
@@ -1,9 +1,73 @@
 import React, { Component } from "react";
+import "./GameChatComponent.css";
+
+const MOCK = [
+  {
+    user: "Nadim",
+    text: "I know how to redux!"
+  },
+  {
+    user: "Kadar",
+    text: "Can I take your picture?"
+  }
+];
 
 class GameChatComponent extends Component {
+  state = {
+    messages: MOCK,
+    input: ""
+  };
+
+  handleChange(e) {
+    this.setState({ input: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const chatMessage = {
+      user: "Kadar",
+      text: this.state.input
+    };
+    this.setState({
+      messages: [...this.state.messages, chatMessage],
+      input: ""
+    });
+  }
+
   render() {
-    // Chat Component
-    return <div>GameChatComponent</div>;
+    const { messages, input } = this.state;
+
+    const displayMessages = messages.map((message, index) => {
+      return (
+        <li key={index} className="chat__item">
+          <div className="chat__text">
+            <span>{message.user}:</span> {message.text}
+          </div>
+        </li>
+      );
+    });
+
+    return (
+      <div className="chat__container">
+        <div className="chat__title">
+          <p>Chat</p>
+        </div>
+
+        <div className="chat__messages__container">
+          <ul className="chat__messages">{displayMessages}</ul>
+        </div>
+
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            onChange={e => this.handleChange(e)}
+            value={input}
+            placeholder="Type your message and press enter..."
+            type="text"
+            className="chat__input"
+          />
+        </form>
+      </div>
+    );
   }
 }
 
