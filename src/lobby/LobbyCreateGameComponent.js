3dc6c1426e83e2f7a1bdbe533a7107d999a63eee
Kadar Mohamed <kadarmohamed@gmail.com>
2019-01-02 10:56:42 +0100

    added create game

--- src/lobby/LobbyCreateGameComponent.js
+++ src/lobby/LobbyCreateGameComponent.js
@@ -3,11 +3,60 @@ import CreateGameForm from './components/CreateGameForm';
 
 
 class LobbyCreateGameComponent extends Component {
+  state = {
+    show: false,
+    rankOn: false,
+    minutes: 3,
+    ratingLimits: {
+      low: 1600,
+      high: 2000
+    },
+    playerColor: ''
+  }
+  showModal = () => {
+    this.setState({ show: true });
+   /*  this.setState({ ...this.state, show: !this.state.show }); */
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+  toggleSwitch = (i) => {
+    //console.log(i)
+    if ((i === 0 && this.state.rankOn) || (i === 1 && !this.state.rankOn)) {
+      this.setState({ ...this.state, rankOn: !this.state.rankOn})
+    }
+  }
+  handleSlider = value => {
+    //console.log(value)
+    this.setState({...this.state, minutes: value})
+  }
+  handleRatingRange = value => {
+    //console.log(value)
+    this.setState({ ...this.state, ratingLimits: {low: value[0] * 25, high: value[1] * 25}})
+  }
+  handleSubmit = (event, i) => {
+    event.preventDefault()
+    console.log(i)
+    this.setState({ ...this.state, playerColor: i, show: !this.state.show })
+    console.log(this.state)
+  }
   render() {
     return (
       <div>
-        Create Game
-        <CreateGameForm />
+        <p onClick={this.showModal}>Create Game</p>
+        <CreateGameForm
+          show={this.state.show}
+          handleClose={this.hideModal} 
+          handleClick={(i) => {this.toggleSwitch(i)}}
+          submitGame={(event, i) => {this.handleSubmit(event, i)}}
+          rankOn={this.state.rankOn} 
+          sliderChange={this.handleSlider}
+          minutes={this.state.minutes}
+          rangeRating={this.handleRatingRange}
+          lowerLimitRating={this.state.ratingLimits.low}
+          upperLimitRating={this.state.ratingLimits.high}
+        />
       </div>
     );
   }
--- src/lobby/components/CreateGameForm.js
+++ src/lobby/components/CreateGameForm.js
@@ -1,28 +1,84 @@
 import React from 'react';
+import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import './CreateGameForm.css';
+import bK from '../../assets/bK.svg'
+import wbK from '../../assets/wbK.svg'
+import wK from '../../assets/wK.svg'
 
-const createGameForm = () => {
-  return (
-    <div>
-      <form>
-        <div>
-          <label>Variant</label>
-          <select name="" id="">
-            <option value="standard">Standard</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="">Tidskontroll</label>
-          <select name="" id="">
-            <option value="realtid">Realtid</option>
-          </select>
-        </div>
-        <div>
-          <div>
-            <label htmlFor="">Minuter per spelare: 1</label>
+
+const createGameForm = (props) => {
+  const showHideClassName = props.show ? "modal display-block" : "modal display-none";
+  let ratingRange = props.rankOn ? (
+    <div className="rangeContainer">
+          <div className="rangelabelContainer">
+            <label htmlFor="">Ratingomfång: <strong>{props.lowerLimitRating} - {props.upperLimitRating}</strong></label>
           </div>
-          <input type="range" min="1" max="9" />
+          <Range
+            allowCross={false}
+            min={40} 
+            defaultValue={[props.lowerLimitRating / 25, props.upperLimitRating / 25]}  
+            onChange={props.rangeRating}
+          />
         </div>
-      </form>
+  ) : null
+  return (
+    <div className={showHideClassName}>
+
+      <div className='main'>
+        <h2>Skapa ett parti</h2>
+        <span onClick={props.handleClose} className="cross">X</span>
+        <form>
+          <div className="variant">
+            <label>Variant</label>
+            <select name="" id="selectVariant">
+              <option value="standard">Standard</option>
+              <option disabled value="crazyHouse">Crazyhouse</option>
+            </select>
+          </div>
+          <div className="timeControl">
+            <label htmlFor="">Tidskontroll</label>
+            <select name="" id="selectTimeControl">
+              <option value="realtid">Realtid</option>
+              <option disabled value="correspondens">Korrespond.</option>
+            </select>
+          </div>
+          <div className="sliderContainer">
+            <div className="labelContainer">
+              <label htmlFor="">Minuter per spelare: <strong>{props.minutes}</strong></label>
+            </div>
+            <Slider
+              defaultValue={3}
+              max={9}
+              min={1}
+              trackStyle={{ backgroundColor: '#d85000', height: 10 }}
+              handleStyle={{
+                borderColor: 'rgb(224, 224, 224)',
+                borderRadius: '1px',
+                height: 28,
+                width: 28,
+                marginLeft: -14,
+                marginTop: -9,
+                backgroundColor: '#eee',
+              }}
+              railStyle={{ backgroundColor: '#fff', height: 12 }}
+              onChange={props.sliderChange}
+              
+            />
+          </div>
+          <div className="switchRankContainer">
+            <div className={!props.rankOn ? "switchRank special" : "switchRank" } onClick={() => {props.handleClick(0)}}>Ej rankat</div>
+            <div className={props.rankOn ? "switchRank special" : "switchRank" } onClick={() => {props.handleClick(1)}}>Rankat</div>
+          </div>
+          {ratingRange}
+          <div className="submitChoice">
+            <button onClick={(event) => {props.submitGame(event, 'bK')}}><img src={bK} alt=""/></button>
+            <button onClick={(event) => {props.submitGame(event, 'wbK')}}><img className="middle" src={wbK} alt=""/></button>
+            <button onClick={(event) => {props.submitGame(event, 'wK')}}><img src={wK} alt=""/></button>
+          </div>
+        </form>
+      </div>
+      
     </div>
   )
 }
