5672f747140fcc5a592757f18566b7da3cfee0be
Khoi Le <minarek@outlook.com>
2019-01-06 21:47:34 +0100

    Removed Heroku server. Only using AWS server.

--- backend/lobby/lobby-client-example/src/config/index.js
+++ backend/lobby/lobby-client-example/src/config/index.js
@@ -1,6 +1,5 @@
 const socketAPI = {
     address: 'http://aws.vlexikon.com:1600', //MongoDB version 4.1.6
-    //address: 'https://gsockapi.herokuapp.com', // MongoDB version 3.6.9
     accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBuYW1lIjoiY2hlc3MiLCJpYXQiOjE1NDU5MDYzMTh9.Jkyy-52zyb7K3xJz4-K3Fr2GmM0dHrxjFThoUTmx4t4'
 };
 
