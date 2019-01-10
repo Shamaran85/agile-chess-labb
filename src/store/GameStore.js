50d9e2602dae9ac19de78cbcb1bd2d0546fca9ce
Nalfs <nadim.al.sharif@gmail.com>
2019-01-10 11:28:26 +0100

    setting up GameStore listener

--- src/store/GameStore.js
+++ src/store/GameStore.js
@@ -1,8 +1,10 @@
 import {BehaviorSubject} from 'rxjs';
+import LobbyStore from '../store/LobbyStore';
 //const Chess = require('chess.js').Chess;
 
 const defaultState = {
   message: 'test',
+  localUserInfo: {}
 };
 
 const subject = new BehaviorSubject(defaultState);
@@ -11,6 +13,17 @@ class GameStore {
     constructor() {
         this.setState({})
     }
+
+    componentDidMount() {
+
+      LobbyStore.getLocalUserInfo().subscribe((data) => {
+          console.log('localUserInfo', data);
+          this.setState({ localUserInfo: data });
+      });
+
+  }
+
+
     setState(st) {
       const val = subject.value;
       this.state = Object.assign({}, val, st);
