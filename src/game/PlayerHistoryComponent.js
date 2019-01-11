a4e9bc5dd8c96c694176a8e9f78ef9fc99b1d927
Abbe <aboutalebabbe@gmail.com>
2019-01-11 13:19:04 +0100

    Added index key for onClick

--- src/game/PlayerHistoryComponent.js
+++ src/game/PlayerHistoryComponent.js
@@ -28,8 +28,8 @@ this.setState({
     
     this.chess.move(history)
 
-    const history = this.state.history.map((move) => {
-      return (<li key={move} onClick={() => this.onClick(move)}>{move.from} / {move.to}</li>)
+    const history = this.state.history.map((move, index) => {
+      return (<li key={index} onClick={() => this.onClick(move)}>{move.from} / {move.to}</li>)
     })
     return (
       <div>
