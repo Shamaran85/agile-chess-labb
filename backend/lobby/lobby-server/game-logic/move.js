5248b7a21498838367f013eda1a29d990a6141be
Niklas Silfverström <niklas@silfverstrom.com>
2019-01-11 13:24:32 +0100

    added todo

--- backend/lobby/lobby-server/game-logic/move.js
+++ backend/lobby/lobby-server/game-logic/move.js
@@ -1,4 +1,12 @@
 let moveArr = [];
+/**
+ * TODO
+ * Add FEN to getRoom
+ * Add function to check if_checkmate for id.
+ * Add player-idn to getRoom.
+ * On checkmate, add boolean to object.
+ * Add function to get all played games between two ids, and returns results.
+ * */
 exports.getRoom = (id, move) => {
     let oldMoves = {
         from: move.from,
