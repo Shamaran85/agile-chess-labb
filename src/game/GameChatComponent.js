5ccba348ea365dec2618d9f602a7e0e7ea276038
Daniel <Shamaran85@gmail.com>
2019-01-15 14:24:44 +0100

    Connected users in chat witb database

--- src/game/GameChatComponent.js
+++ src/game/GameChatComponent.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import "./GameChatComponent.css";
 
 import ChatStore from "../store/ChatStore";
+import LobbyStore from "../store/LobbyStore";
+import { socketAPI } from "../config/";
+import { get } from "https";
 
 class GameChatComponent extends Component {
   constructor(props) {
@@ -9,7 +12,8 @@ class GameChatComponent extends Component {
 
     this.state = {
       chatMessage: "",
-      messages: []
+      messages: [],
+      userId: null
     };
   }
 
@@ -19,6 +23,37 @@ class GameChatComponent extends Component {
         messages: [...this.state.messages, payload.message]
       });
     });
+
+    LobbyStore.getLocalUserInfo().subscribe(payload => {
+      const userId = payload._id;
+      this.setState({ userId }, () => {
+        this.fetchUser();
+      });
+    });
+  }
+
+  fetchUser() {
+    const userId = this.state.userId;
+    const url = "http://aws.vlexikon.com:1600/users/" + userId;
+
+    if (userId === undefined) {
+      return null;
+    }
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + socketAPI.accessToken
+      }
+    })
+      .then(result => result.json())
+      .then(res => {
+        console.log(url);
+        console.log("Resultat", res.name);
+        this.setState({ userName: res.name });
+      })
+      .catch(error => console.log(error));
   }
 
   handleMessageInput(e) {
@@ -28,10 +63,10 @@ class GameChatComponent extends Component {
   handleClick(e) {
     e.preventDefault();
 
-    // Mock Data
+    // User Data
     const data = {
-      userId: 123,
-      userName: "Kadar",
+      userId: this.state.userId,
+      userName: this.state.userName,
       userMessage: this.state.chatMessage
     };
 
@@ -43,7 +78,16 @@ class GameChatComponent extends Component {
   }
 
   render() {
-    const { messages, chatMessage } = this.state;
+    const { messages, chatMessage, userName, userId } = this.state;
+
+    if (userName !== undefined) {
+      console.log("USER:", userName);
+    }
+
+    // const test = userId ? <span>{data.userName}:</span> : <span>Kalle:</span>;
+
+    if (userId) {
+    }
 
     const displayMessages = messages.map((data, index) => {
       return (
