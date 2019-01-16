c12454df291d57dd216fed4e8a9ab98f75a85c14
Jakob <jakob9310@live.se>
2019-01-16 08:28:40 +0100

    fixed merge

--- src/lobby/LobbyComponent.js
+++ src/lobby/LobbyComponent.js
@@ -4,15 +4,22 @@ import LobbyFilterComponent from './LobbyFilterComponent';
 import LobbySeeksComponent from './LobbySeeksComponent';
 import LobbyCreateGameComponent from './LobbyCreateGameComponent';
 import LobbyLoginComponent from './LobbyLogInComponent'
+import './LobbyComponent.css'
 
 class LobbyComponent extends Component {
   render() {
     return (
-      <div>
+      <div className='LCmain'>
+        <div className='LCtopbar'>
+          <div className='LCcreategame'>
+            <LobbyCreateGameComponent />
+          </div>
+          <div className='LClogin'>
+            <LobbyLoginComponent />
+          </div>
+        </div>
         <LobbySeeksComponent />
         <LobbyFilterComponent />
-        <LobbyCreateGameComponent />
-        <LobbyLoginComponent />
       </div>
     );
   }
